21af985a8f551adfa00ec6f6c55c77b4b6925d5e
Paul Spencer <paul@pagameba.com>
2012-10-03 08:40:28 -0400

    adding initial implementation.

---
+++ MapGL/MapGLTests/MapGLTests.h
@@ -0,0 +1,13 @@
+//
+//  MapGLTests.h
+//  MapGLTests
+//
+//  Created by Paul Spencer on 2012-10-03.
+//  Copyright (c) 2012 DM Solutions Group Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MapGLTests : SenTestCase
+
+@end
---
+++ MapGL/MapGLTests/MapGLTests.m
@@ -0,0 +1,32 @@
+//
+//  MapGLTests.m
+//  MapGLTests
+//
+//  Created by Paul Spencer on 2012-10-03.
+//  Copyright (c) 2012 DM Solutions Group Inc. All rights reserved.
+//
+
+#import "MapGLTests.h"
+
+@implementation MapGLTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MapGLTests");
+}
+
+@end
